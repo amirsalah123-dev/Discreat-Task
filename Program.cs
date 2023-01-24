94cc3b0b30b8c323cc0e7ae260ae289325aece06
amirsalah123-dev <123492191+amirsalah123-dev@users.noreply.github.com>
2023-01-24 19:31:05 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool isPrime = true;
+            Console.WriteLine("prime numbers :");
+
+            {
+                for (int i = 2; i <= 100; i++)
+                {
+                    for (int j = 2; j < 100; j++)
+                    {
+                        if (i != j && i % j == 0)
+                        {
+                            isPrime= false;
+
+                            break;
+
+
+
+                            {
+                                if (isPrime)
+                                {
+                                    Console.Write("\t" + i);
+                                    {
+                                        isPrime = true;
+                                        {
+
+
+                                            Console.ReadKey();
+                                            {
+
+
+
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+
+                }
+            }
+        
+
+        }
+    }
+}
